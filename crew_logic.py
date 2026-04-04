703602d52ed16baa3c7a64de10f7a19603c9432b
sharanakuama <snmoni1999@gmail.com>
2026-04-05 00:12:53 +0530

    https://github.com/SharanakumaraMoni/AI-Resume-Analyzer-Agent.git

--- crew_logic.py
+++ crew_logic.py
@@ -1,14 +1,27 @@
 from crewai import Crew
 from tasks import create_tasks
+from agents import resume_reader, job_analyzer, matcher
+import time
 
 def run_crew(resume_text, job_desc):
+    
+    # ✅ Create tasks
     tasks = create_tasks(resume_text, job_desc)
 
+    # ✅ Create crew (IMPORTANT)
     crew = Crew(
-        agents=[task.agent for task in tasks],
+        agents=[resume_reader, job_analyzer, matcher],
         tasks=tasks,
-          verbose=True
+        verbose=True
     )
 
-    result = crew.kickoff()
-    return result
+    try:
+        result = crew.kickoff()
+        return result
+    except Exception as e:
+        if "Rate limit" in str(e):
+            time.sleep(12)
+            result = crew.kickoff()
+            return result
+        else:
+            raise e
